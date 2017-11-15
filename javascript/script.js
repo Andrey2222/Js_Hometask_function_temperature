26444afbf27420a5ce12f883fd7ed80ac7d27a6f
John Doe <johndoe@example.com>
2017-11-15 16:49:54 +0200

    fics mistake

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,26 @@
-function fromCelsiustoKelvin(){
-	var kelvin = 273 + celsiy_K;
-	alert("Вы ввели " + celsiy_K +"°C= " + kelvin + "°К")
-	return;
+function TakeCelsiusGettoKelvin(celsiy_K){
+	var Constant_kelvin = 273;
+	var kelvin = Constant_kelvin + celsiy_K;
+	return kelvin + "Градусов кельвина";
 }
-function fromCelsiustoFarengeight(){
-	var farengeight = (celsiy_F *1.8) + 32;
-	alert("Вы ввели " + celsiy_F +"°C= " + farengeight + "°F")
-	return;
+function TakeCelsiusGetFarengeight(celsiy_F){
+	var Constant_farengeight = 1.8;
+	var Constant_farengeight2 = 32;
+	var farengeight = (celsiy_F *Constant_farengeight) + Constant_farengeight2;
+	return farengeight + "Градусов фаренгейта";
 }
 
-var ask = prompt("для преобразования градусов цельсия в градусы кельвина нажмите 'Кельвин" + "\n" + "Дляпреобразования градусов цельсия в фаренгейты нажмите 'Фаренгейт").toLowerCase();
-if (ask == "кельвин"){
-	var celsiy_K = +prompt("Введите числом количество градусов цельсия которое вы желаете перевести в кельвины");
-		if (isNaN(celsiy_K)){
-			alert("Вы ввели неверные данные");
-		}else{
-			fromCelsiustoKelvin();
-		}
-}else if (ask == "фаренгейт"){
-	var celsiy_F = +prompt("для преобразования введите количество градусов цельсия которые хотите перевести в градусы фаренгейта");
-		if (isNaN(celsiy_F)){
-			alert("Вы ввели неверные данные");
-		}else{
-			fromCelsiustoFarengeight();
-		}
-}else{
+
+
+function WorkProgram(){
+	var ask = prompt("для преобразования градусов цельсия в градусы кельвина нажмите 'Кельвин" + "\n" + "Дляпреобразования градусов цельсия в фаренгейты нажмите 'Фаренгейт").toLowerCase();
+	if (ask == "кельвин"){
+		alert(TakeCelsiusGettoKelvin(5));
+	}else if (ask == "фаренгейт"){
+		alert(TakeCelsiusGetFarengeight(3));
+	}else{
 	alert("введеные данные не верны");
+	}
 }
 
+WorkProgram();
