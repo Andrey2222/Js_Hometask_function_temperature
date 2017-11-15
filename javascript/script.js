0e2a6b1e14defe1e3ea15e065b5026b17d6bd625
John Doe <johndoe@example.com>
2017-11-15 19:27:53 +0200

    fics camelcase

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,25 @@
-function TakeCelsiusGettoKelvin(celsiy_K){
-	var Constant_kelvin = 273;
-	var kelvin = Constant_kelvin + celsiy_K;
-	return kelvin + "Градусов кельвина";
+function takeCelsiusGettoKelvin(celsiy_K){
+	var CONSTANT_BOIL_K = 273;
+	return CONSTANT_BOIL_K + celsiy_K
 }
-function TakeCelsiusGetFarengeight(celsiy_F){
-	var Constant_farengeight = 1.8;
-	var Constant_farengeight2 = 32;
-	var farengeight = (celsiy_F *Constant_farengeight) + Constant_farengeight2;
-	return farengeight + "Градусов фаренгейта";
+function takeCelsiusGetFarengeight(celsiy_F){
+	var CONSTANT = 1.8;
+	var CONSTANT_BOIL_F = 32;
+	var farengeight = (celsiy_F *CONSTANT) + CONSTANT_BOIL_F;
+	return farengeight  
 }
 
 
 
-function WorkProgram(){
+function workProgram(){
 	var ask = prompt("для преобразования градусов цельсия в градусы кельвина нажмите 'Кельвин" + "\n" + "Дляпреобразования градусов цельсия в фаренгейты нажмите 'Фаренгейт").toLowerCase();
 	if (ask == "кельвин"){
-		alert(TakeCelsiusGettoKelvin(5));
+		alert(takeCelsiusGettoKelvin(5));
 	}else if (ask == "фаренгейт"){
-		alert(TakeCelsiusGetFarengeight(3));
+		alert(takeCelsiusGetFarengeight(3));
 	}else{
-	alert("введеные данные не верны");
+		alert("введеные данные не верны");
 	}
 }
 
-WorkProgram();
+workProgram();
